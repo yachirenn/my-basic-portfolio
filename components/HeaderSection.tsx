8cf66c9f14c26517d4931f7048b351b93cad0487
yachirenn <rendysulistyawan11@gmail.com>
2026-01-08 21:46:52 +0700

    update env'

---
+++ components/HeaderSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeaderSection() {
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <h1 className="text-2xl font-bold">My App</h1>
+        </header>
+    );
+}
