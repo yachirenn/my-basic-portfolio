a21259983198945ec6c4bdcf7224512e4ba407d3
yachirenn <rendysulistyawan11@gmail.com>
2026-01-12 20:09:03 +0700

    recognition components

--- src/components/desktop/DesktopIcon.tsx
+++ src/components/desktop/DesktopIcon.tsx
@@ -1,11 +1,12 @@
 interface DesktopIconsProps {
   icon: string;
   label: string;
+  onClick: () => void;
 }
 
-export default function DesktopIconsProps() {
+export default function DesktopIconsProps({ icon, label, onClick }: DesktopIconsProps) {
   return (
-    <div className="desktop-icon">
+    <div className="desktop-icon" onClick={onClick}>
       <div className="desktop-icon-box">{icon}</div>
       <span className="desktop-icon-label">{label}</span>
     </div>
--- src/components/desktop/TerminalWindows.tsx
+++ src/components/desktop/TerminalWindows.tsx
@@ -1,7 +1,15 @@
 export default function TerminalWindow() {
+  const span = ">"
   return(
-    <div className="terminal-placeholder">
-      Terminal
+    <div className="terminal-window">
+      <div className="terminal-header">
+        Terminal
+      </div>
+
+      <div className="terminal-body">
+        <p> {span} Welcome to my portfolio</p>
+        <p> {span} type <span className="terminal-command">/help</span></p>
+      </div>
     </div>
   );
 }
