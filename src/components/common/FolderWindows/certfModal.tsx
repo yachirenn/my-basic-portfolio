4e3dabc65337a3fccbecbd51b7b7c260b6fb6e79
yachirenn <rendysulistyawan11@gmail.com>
2026-01-29 19:11:17 +0700

    added a certf page section

---
+++ src/components/common/FolderWindows/certfModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import FolderModal from "@/components/common/FolderModals";
+import {  } from "@/constants/skills";
+import { motion } from "motion/react";
+
+export default function CertfModal() {
+  return 
+  <>
+  </>
+}
