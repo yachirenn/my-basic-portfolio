4c0ab53ba5f47586ebb662f70ddad1bb1808a498
yachirenn <rendysulistyawan11@gmail.com>
2026-02-16 23:58:14 +0700

    refactor AboutSection component layout and content

--- src/components/mobile/sections/AboutSection.tsx
+++ src/components/mobile/sections/AboutSection.tsx
@@ -1,19 +1,13 @@
 "use client"
 
+import HeroPixelGrid from "@/components/ui/pixelgrid";
 import { motion } from "framer-motion";
-import HeroCanvas from "./HeroCanvas";
 
 export default function AboutSection() {
   return (
-    <section className="relative min-h-screen px-6 pt-18 pb-10 overflow-hidden">
-      <HeroCanvas />
+    <div className="relative px-6 pt-18 pb-10 overflow-hidden">
       <div className="absolute inset-0 pointer-events-none">
-        <motion.div
-          initial={{opacity: 0}}
-          animate={{opacity: 0.5}}
-          transition={{duration: .875}}
-          className="absolute top-0 left-0 w-full h-40 bg-[radiant-gradient(circle, #ffffff22_1px, transparent_1px)] bg-size-[10px_10px] mask-image-gradient"
-        />
+        <HeroPixelGrid />
       </div>
 
       {/* CONTENT */}
@@ -23,8 +17,13 @@ export default function AboutSection() {
           <img className="aspect-square w-full h-full object-cover" src="/images/acheroninfinity.png" alt="yachirenn's photo" />
         </span>
 
-        <h1 className="text-4xl font-bold pb-6">Hi! I'm Renn</h1>
-        <p className="text-gray-400 text-lg font-mono">
+        <h1 className="text-4xl font-bold pb4">Hi! I'm Renn</h1>
+        <p className="text-gray-400 text-lg font-mono pb-10">
+          Student want to be a Web Developers. I love make a beautifull and functional websites. Currently I am learning about React and REST API.
+        </p>
+
+        <h1 className="text-4xl font-bold">About Me_</h1>
+        <p className="text-gray-400 pt-4 text-lg font-mono">
           Hello, I am yachirenn. Rendy Sulistyawan, commonly known as Rendy. 
           I am a student majoring in System, Information, Network, and Application, 
           with a particular interest in web development. 
@@ -32,11 +31,7 @@ export default function AboutSection() {
           particularly using JavaScript, and I am eager to continue learning about the latest technologies. 
           I am seeking opportunities to contribute in an environment that fosters creativity and innovation.
         </p>
-
-        <p className="text-gray-400 pt-6 text-lg font-mono">
-          Curently I am learning about React and REST API.
-        </p>
       </div>
-    </section>
+    </div>
   );
 }
