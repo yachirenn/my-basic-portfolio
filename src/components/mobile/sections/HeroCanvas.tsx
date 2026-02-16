54a0ce77cd6c6aa8542564997b5cb4281d1f0042
yachirenn <rendysulistyawan11@gmail.com>
2026-02-16 23:36:04 +0700

    create a github graph log and random

--- src/components/mobile/sections/HeroCanvas.tsx
+++ src/components/mobile/sections/HeroCanvas.tsx
@@ -1,77 +1,10 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import HeroPixelGrid from "@/components/ui/pixelgrid";
 
-interface Dot {
-  x: number;
-  y: number;
-  speed: number;
-}
-
-export default function HeroCanvas() {
-
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-
-    const canvasEl = canvasRef.current;
-    if (!canvasEl) return;
-
-    const context = canvasEl.getContext("2d");
-    if (!context) return;
-
-    // sekarang TS tahu ini NON NULL
-    const canvas = canvasEl;
-    const ctx = context;
-
-    let animationId: number;
-
-    const resizeCanvas = () => {
-      canvas.width = canvas.offsetWidth;
-      canvas.height = canvas.offsetHeight;
-    };
-
-    resizeCanvas();
-    window.addEventListener("resize", resizeCanvas);
-
-    const dots: Dot[] = Array.from({ length: 120 }).map(() => ({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      speed: Math.random() * 0.5 + 0.2,
-    }));
-
-    function animate() {
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      dots.forEach(d => {
-        d.y += d.speed;
-
-        if (d.y > canvas.height) {
-          d.y = 0;
-          d.x = Math.random() * canvas.width;
-        }
-
-        ctx.fillStyle = "rgba(255,255,255,0.15)";
-        ctx.fillRect(d.x, d.y, 1, 1);
-      });
-
-      animationId = requestAnimationFrame(animate);
-    }
-
-    animate();
-
-    return () => {
-      cancelAnimationFrame(animationId);
-      window.removeEventListener("resize", resizeCanvas);
-    };
-
-  }, []);
-
+export default function HeroSection( { children }: { children: React.ReactNode } ) {
   return (
-    <canvas
-      ref={canvasRef}
-      className="absolute top-0 left-0 w-full h-full pointer-events-none"
-    />
+    // BACKGROUND ANIMATION
+    <HeroPixelGrid />
   );
 }
---
+++ src/components/ui/pixelgrid.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function HeroPixelGrid() {
+
+  const rows = 7
+  const cols = 52
+  const [active, setActive] = useState<number[]>([])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newActive = Array.from({ length: 40 }, () =>
+        Math.floor(Math.random() * rows * cols)
+      )
+      setActive(newActive)
+    }, 800)
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="w-full h-full absolute inset-0 flex items-start justify-center opacity-55 pointer-events-none">
+      <div className="grid gap-1" style={{ gridTemplateColumns: `repeat(${cols}, 10px)`}}>
+        {Array.from({ length: rows * cols }).map((_, i) => {
+          const isActive = active.includes(i)
+          return (
+            <div
+              key={i}
+              className={`
+                w-2.5 h-2.5 
+                transition-all duration-500
+                ${isActive
+                  ? "bg-green-500 shadow-[0_0_8px_rgba(65,195,10,0.7)] scale-110"
+                  : "bg-green-900/30"}
+              `}
+            />
+          )
+        })}
+
+      </div>
+
+    </div>
+  )
+}
